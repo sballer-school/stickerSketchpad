84e31b70290b2a541aebe8b538603fff694a0696
sballer-school <sballer@ucsc.edu>
2025-10-24 21:30:56 +0000

    Drawing is now done via events and points (step 3 completed)

--- src/main.ts
+++ src/main.ts
@@ -19,35 +19,69 @@ document.body.appendChild(clearCanvas);*/
 
 const ctx = canvas.getContext("2d");
 
+const lines: { x: number; y: number }[][] = [];
+const redoLines: { x: number; y: number }[] = [];
+
+let currentLine: { x: number; y: number }[] | null = null;
+
 const cursor = { active: false, x: 0, y: 0 };
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  canvas.dispatchEvent(changedEvent);
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active && ctx != null) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+  if (cursor.active && currentLine != null) {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+    canvas.dispatchEvent(changedEvent);
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
 });
 
+const changedEvent = new CustomEvent("build");
+
+canvas.addEventListener("build", () => {
+  redraw();
+});
+
+function redraw() {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const line of lines) {
+      if (line.length > 1) {
+        ctx.beginPath();
+        const { x, y } = line[0];
+        ctx.moveTo(x, y);
+        for (const { x, y } of line) {
+          ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+document.body.append(document.createElement("br"));
+
 const clearButton = document.createElement("button");
-clearButton.innerHTML = "clear";
+clearButton.innerHTML = "Clear";
 document.body.append(clearButton);
 
 clearButton.addEventListener("click", () => {
-  if (ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  lines.splice(0, lines.length);
+  canvas.dispatchEvent(changedEvent);
 });
